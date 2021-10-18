95ab5135cc9dace2c49b053fb63bedc6448561f8
Irina.Petrovskaya <irina.petrovskaya@jetbrains.com>
2021-10-19 01:10:40 +0300

    Initial commit

---
+++ src/main/java/com/example/springbootconfigimport/ConfProps2.java
@@ -0,0 +1,31 @@
+package com.example.springbootconfigimport;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * *
+ * <p>Created by irina on 19.10.2021.</p>
+ * <p>Project: spring-boot-config-import</p>
+ * *
+ */
+@ConfigurationProperties("bar")
+public class ConfProps2 {
+    String prop1;
+    String prop2;
+
+    public String getProp1() {
+        return prop1;
+    }
+
+    public void setProp1(String prop1) {
+        this.prop1 = prop1;
+    }
+
+    public String getProp2() {
+        return prop2;
+    }
+
+    public void setProp2(String prop2) {
+        this.prop2 = prop2;
+    }
+}
---
+++ src/test/java/com/example/springbootconfigimport/SpringBootConfigImportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootconfigimport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootConfigImportApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
